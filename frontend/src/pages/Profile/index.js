a7efa496df10f48ec60d15a2ca295d26818f1a4d
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-03-26 23:48:45 -0300

    tratamento da lista de casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -46,11 +46,43 @@ export default function Profile() {
     history.push('/');
   }
 
+  function List() {
+    if (incidents.length) {
+      return (
+        <ul>
+          {incidents.map(incident => (
+            <li key={incident.id}>
+              <strong>CASO:</strong>
+              <p>{incident.title}</p>
+
+              <strong>DESCRIÇÃO:</strong>
+              <p>{incident.description}</p>
+
+              <strong>VALOR:</strong>
+              <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+
+              <button onClick={() => handleDeleteIncident(incident.id)} type="button">
+                <FiTrash2 size={20} color="#A8A8B3"></FiTrash2>
+              </button>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+    if (!incidents.length) {
+      return (
+        <div className="no-incidents-card">
+          <p>A sua ONG ainda não possui casos cadastrados no sistema. <span>Cadastre um caso!</span></p>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero"></img>
-        <span>Bem vinda, {ongName}</span>
+        <span>Portal {ongName}</span>
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#E02041"></FiPower>
@@ -59,24 +91,7 @@ export default function Profile() {
 
       <h1>Casos cadastrados</h1>
 
-      <ul>
-        {incidents.map(incident => (
-          <li key={incident.id}>
-            <strong>CASO:</strong>
-            <p>{incident.title}</p>
-
-            <strong>DESCRIÇÃO:</strong>
-            <p>{incident.description}</p>
-
-            <strong>VALOR:</strong>
-            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
-
-            <button onClick={ () => handleDeleteIncident(incident.id) } type="button">
-              <FiTrash2 size={20} color="#A8A8B3"></FiTrash2>
-            </button>
-          </li>
-        ))}
-      </ul>
+      <List></List>
 
     </div>
   );
